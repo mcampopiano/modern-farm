4991e45be0f041f52252fba4ff77982608047515
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-08 14:25:49 -0500

    created .js files for each type of plant and wrote functions in each that create the seed objects for said plant.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,12 @@
 import {createPlan} from "./plan.js"
-console.log("Welcome to the main module")
+import {createAsparagus} from "./seeds/asparagus.js"
+import {createCorn} from "./seeds/corn.js"
+import {createPotato} from "./seeds/potato.js"
+import {createSoybean} from "./seeds/soybean.js"
+import {createSunflower} from "./seeds/sunflower.js"
+import {createWheat} from "./seeds/wheat.js"
 
+const potatoSeed = createPotato()
 const yearlyPlan = createPlan();
 
-console.log(yearlyPlan);
+console.log(potatoSeed)
