30bdbb4f9419ccac3bc0e9fe0a1e510fddfc8f17
Mario Campopiano <mcampopiano92@gmail.com>
2020-10-11 15:26:34 -0500

    Tried a few things to get code to pass npm test. added a forEach in the tractor module on the corn. No luck.

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,15 +1,8 @@
 let field = []
 
 export const addPlant = (seedObj) => {
-    // Corn is an array, so this extra conditional is necearray to make it iterate through that array.
-    if (Array.isArray(seedObj)) {
-        for (const seed of seedObj) {
-            field.push(seed)
-        }
-    } else {
-        field.push(seedObj)
+     return field.push(seedObj)
     }
-}
 
 export const usePlant = () => {
     return field.slice()
--- src/scripts/tractor.js
+++ src/scripts/tractor.js
@@ -12,23 +12,24 @@ export const plantSeeds = (annualPlan) => {
     for (const row of annualPlan) {
         for (const plant of row ) {
             if (plant === "Wheat") {
-                // const wheatSeed = createWheat()
-                addPlant(createWheat())
+                const wheatSeed = createWheat()
+                addPlant(wheatSeed)
             } else if (plant === "Asparagus") {
-                // const asparagusSeed = createAsparagus()
-                addPlant(createAsparagus())
+                const asparagusSeed = createAsparagus()
+                addPlant(asparagusSeed)
             } else if (plant === "Corn") {
-                // const cornSeed = createCorn()
-                addPlant(createCorn())
+                const cornSeed = createCorn()
+                cornSeed.forEach((corn) => addPlant(corn))
+                // addPlant(cornSeed)
             } else if (plant === "Potato") {
-                // const potatoSeed = createPotato()
-                addPlant(createPotato())
+                const potatoSeed = createPotato()
+                addPlant(potatoSeed)
             } else if (plant === "Soybean") {
-                // const soybeanSeed = createSoybean()
-                addPlant(createSoybean())
+                const soybeanSeed = createSoybean()
+                addPlant(soybeanSeed)
             } else if (plant === "Sunflower") {
-                // const sunflowerSeed = createSunflower()
-                addPlant(createSunflower())
+                const sunflowerSeed = createSunflower()
+                addPlant(sunflowerSeed)
             }
         }
     }
